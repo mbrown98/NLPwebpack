c6df6e34033b785c3c0734ef45346c1ca6b0f27a
Matt Brown <matthewbrown0798@gmail.com>
2020-12-07 14:09:21 -0500

    installed babel, 2.9 done

--- webpack.config.js
+++ webpack.config.js
@@ -2,4 +2,11 @@ const path = require("path");
 const webpack = require("webpack");
 module.exports = {
   entry: "./src/client/index.js",
+  rules: [
+    {
+      test: "/.js$/",
+      exclude: /node_modules/,
+      loader: "babel-loader",
+    },
+  ],
 };
