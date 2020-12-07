653a0b60e363a367f811d075210661821a569a59
Matt Brown <matthewbrown0798@gmail.com>
2020-12-07 14:12:48 -0500

    fixed error in webpack config, all js files showing up as expected

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,13 @@ const path = require("path");
 const webpack = require("webpack");
 module.exports = {
   entry: "./src/client/index.js",
-  rules: [
-    {
-      test: "/.js$/",
-      exclude: /node_modules/,
-      loader: "babel-loader",
-    },
-  ],
+  module: {
+    rules: [
+      {
+        test: "/.js$/",
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
 };
